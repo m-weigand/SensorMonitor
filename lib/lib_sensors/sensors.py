7203e90016bac4ad323f8fe72e578a2287ff985c
Maximilian Weigand <mweigand@geo.uni-bonn.de>
2014-11-24 21:34:28 +0100

    add moisture sensor

--- lib/lib_sensors/sensors.py
+++ lib/lib_sensors/sensors.py
@@ -8,3 +8,4 @@ available_loggers = {}
 available_loggers['datetime'] = datelogger.DateLogger
 available_loggers['tf_light'] = tf_light.tf_light
 available_loggers['tf_temp'] = tf_light.tf_temp
+available_loggers['tf_moisture'] = tf_light.tf_moisture
